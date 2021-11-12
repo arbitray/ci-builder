2c14f70f04b3e5b52e2870ffaec216bf789a820b
zhangh <wittcnezh@foxmail.com>
2021-11-12 13:00:06 +0800

    fix go sh issue

--- hack/go_install_utils.sh
+++ hack/go_install_utils.sh
@@ -7,10 +7,10 @@ echo $ARCH
 
 # GOLANG
 if [[ ${ARCH} == 'x86_64' ]]; then
-  curl -f -L https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz|tar -C /usr/local -xz && \
+  curl -f -L https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz|tar -C /usr/local -xz
 elif [[ ${ARCH} == 'aarch64' ]]
 then
-  curl -f -L https://golang.org/dl/go$GOLANG_VERSION.linux-arm64.tar.gz|tar -C /usr/local -xz && \
+  curl -f -L https://golang.org/dl/go$GOLANG_VERSION.linux-arm64.tar.gz|tar -C /usr/local -xz
 else
   echo "do not support this arch"
   exit 1
